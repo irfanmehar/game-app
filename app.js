4deb75d518410278516f6d4146203143b1af2f00
Irfan <irfan@coursera-something.com>
2018-07-23 11:29:31 -0700

    new version of filee

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 ( function(){
 
-  'use strict';
-
-  angular.module('myFirstApp',[])
-
-  .controller('myFirstController', function ($scope) {
-  $scope.name = "M.irfan";
-
-  $scope.sayhello = function () {
-
-    return "welcome sir";
-
-  };
 
 
-  } );
+  angular.module('RoutingApp',['ui.router']);
+
+   angular.module('RoutingApp')
+   .config(RoutesConfig);
+
+    RoutesConfig.$inject =['$stateProvider','$urlRouteprovider'];
+    function RoutesConfig($stateProvider ,$urlRouteprovider) {
+
+    }
+
+
+
+
+
+
+
+
 
 
 
